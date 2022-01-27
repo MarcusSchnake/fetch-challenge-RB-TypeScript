7568a6a577df47f798c33705f269279c788b308d
Marcus A. Schnake <marcus.schnake@gmail.com>
2022-01-26 21:05:44 -0500

    Final code

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Fetch from "./components/ApiFetch";
+
+function App() {
+  return (
+    <div className="App">
+      <Fetch />
+    </div>
+  );
+}
+	
+
+export default App;
